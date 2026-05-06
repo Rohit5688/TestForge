971591d61390f5c21efbd97c22a92b0832c529dc
rohit5688 <rohit.sakhawalkar@gmail.com>
2026-05-07 00:47:50 +0530

    Cleanup: Remove compiled files, cache, and scratch files from tracking

--- src/interfaces/ICodebaseAnalyzer.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=ICodebaseAnalyzer.js.map
--- src/services/execution/TestRunnerService.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ITestRunner, TestRunnerResult } from '../interfaces/ITestRunner.js';
-export declare class TestRunnerService implements ITestRunner {
-    runTests(projectRoot: string, specificTestArgs?: string): Promise<TestRunnerResult>;
-}
-//# sourceMappingURL=TestRunnerService.d.ts.map
